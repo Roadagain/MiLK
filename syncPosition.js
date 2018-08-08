6a28abea8b4fc8de7372e0b6b830ca3b3ea7aaf8
Roadagain <ringoh72@gmail.com>
2018-08-09 00:10:57 +0900

    ちょっとスタイル崩れるけどレスポンシブにする

--- syncPosition.js
+++ syncPosition.js
@@ -3,9 +3,9 @@ const moveFace = (x) => {
   const mouth = document.getElementsByClassName('mouth')[0];
 
   const rotateDeg = x / 300 * 45;
-  const translateX = x / 300 * 45;
-  brows[0].style.transform = 'translateX(' + translateX + 'px)';
-  brows[1].style.transform = 'translateX(' + translateX + 'px)';
+  const translateX = x / 300 * 30;
+  brows[0].style.transform = 'translateX(' + translateX + '%)';
+  brows[1].style.transform = 'translateX(' + translateX + '%)';
   mouth.style.transform = 'rotate(' + rotateDeg + 'deg)';
 };
 
